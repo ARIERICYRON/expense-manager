90ba305a8eb5dd939d3ebe1fb1d39aa89832164d
cyron arieri <ariericyron@gmail.com>
2024-10-06 18:25:21 -0400

    set up routing with react router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import ExpenseList from './components/ExpenseList';
+import AddExpense from './components/AddExpense';
+import EditExpense from './components/EditExpense';
+import ExpenseSummary from './components/ExpenseSummary';
+import { ExpenseProvider } from './context/ExpenseContext';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ExpenseProvider>
+      <Router>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/add">Add Expense</Link></li>
+            <li><Link to="/summary">Summary</Link></li>
+          </ul>
+        </nav>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<ExpenseList />} />
+            <Route path="/add" element={<AddExpense />} />
+            <Route path="/edit/:id" element={<EditExpense />} />
+            <Route path="/summary" element={<ExpenseSummary />} />
+          </Routes>
+        </div>
+      </Router>
+    </ExpenseProvider>
   );
 }
 
