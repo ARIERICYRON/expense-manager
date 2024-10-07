604946b976b41b09475ca61b9517eed531246124
cyron arieri <ariericyron@gmail.com>
2024-10-06 21:12:42 -0400

    styled my navbar

--- src/App.js
+++ src/App.js
@@ -5,26 +5,27 @@ import AddExpense from './components/AddExpense';
 import EditExpense from './components/EditExpense';
 import ExpenseSummary from './components/ExpenseSummary';
 import { ExpenseProvider } from './context/ExpenseContext';
+import { AppBar, Toolbar, Button, Box } from '@mui/material';
 
 function App() {
   return (
     <ExpenseProvider>
       <Router>
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/add">Add Expense</Link></li>
-            <li><Link to="/summary">Summary</Link></li>
-          </ul>
-        </nav>
-        <div className="App">
+      <AppBar position="static">
+          <Toolbar>
+            <Button color="inherit" component={Link} to="/">Home</Button>
+            <Button color="inherit" component={Link} to="/add">Add Expense</Button>
+            <Button color="inherit" component={Link} to="/summary">Summary</Button>
+          </Toolbar>
+        </AppBar>
+        <Box sx={{ padding: 3 }}>
           <Routes>
             <Route path="/" element={<ExpenseList />} />
             <Route path="/add" element={<AddExpense />} />
             <Route path="/edit/:id" element={<EditExpense />} />
             <Route path="/summary" element={<ExpenseSummary />} />
           </Routes>
-        </div>
+        </Box>
       </Router>
     </ExpenseProvider>
   );
